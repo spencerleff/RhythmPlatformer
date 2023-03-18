f6d95c80513680110cefc6bf769dce31392af45e
spencerleff <sdleff@mail.csuchico.edu>
2023-03-17 21:01:24 -0700

    R1 completed (Pause menu was the last item).  R2 almost finished.  just need to fine tune animtaions and add the actual song with note timings.  notes can be generated at any time and run at any speed.  I also need to make the background repeatedly move downwards at the same speed as the notes so the game looks a little better

---
+++ Assets/DefaultControls.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultControls : MonoBehaviour
+{
+    void Start()
+    {
+        //if controls have not been set, then make them the default
+        if(!PlayerPrefs.HasKey("Control1"))
+        {
+            PlayerPrefs.SetInt("Control1", (int)KeyCode.D);
+        }
+
+        if(!PlayerPrefs.HasKey("Control2"))
+        {
+            PlayerPrefs.SetInt("Control2", (int)KeyCode.F);
+        }
+
+        if(!PlayerPrefs.HasKey("Control3"))
+        {
+            PlayerPrefs.SetInt("Control3", (int)KeyCode.J);
+        }
+
+        if(!PlayerPrefs.HasKey("Control4"))
+        {
+            PlayerPrefs.SetInt("Control4", (int)KeyCode.K);
+        }
+
+        //initial volume slider value is 1
+        if(!PlayerPrefs.HasKey("Volume"))
+        {
+            PlayerPrefs.SetFloat("Volume", 1);
+        }
+
+        //by default, pause screen is inactive
+        PlayerPrefs.SetInt("PauseActive", 0);
+    }
+}
